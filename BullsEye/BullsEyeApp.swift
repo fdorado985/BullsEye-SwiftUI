df3f5938b3489abb394514a46e36cb93ac6626bc
Juan Dorado <fdorado985@gmail.com>
2020-12-20 13:56:33 -0600

    Fix Swiftlint generated warnings

--- BullsEye/BullsEyeApp.swift
+++ BullsEye/BullsEyeApp.swift
@@ -9,9 +9,9 @@ import SwiftUI
 
 @main
 struct BullsEyeApp: App {
-    var body: some Scene {
-        WindowGroup {
-            ContentView()
-        }
+  var body: some Scene {
+    WindowGroup {
+      ContentView()
     }
+  }
 }
