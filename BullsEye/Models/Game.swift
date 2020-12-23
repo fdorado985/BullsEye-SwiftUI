b6ae1d5e31931243367b500dfffe4af19dd8dce5
Juan Dorado <fdorado985@gmail.com>
2020-12-22 23:16:52 -0600

    Organize the app and create the model to handle the game logic

---
+++ BullsEye/Models/Game.swift
@@ -0,0 +1,14 @@
+//
+//  Game.swift
+//  BullsEye
+//
+//  Created by Juan Francisco Dorado Torres on 22/12/20.
+//
+
+import Foundation
+
+struct Game {
+  var target: Int = 42
+  var score: Int = 0
+  var round: Int = 1
+}
