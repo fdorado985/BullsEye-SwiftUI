cc7c9e2ae334f44cd201979811a25b9e81c3ba24
Juan Dorado <fdorado985@gmail.com>
2020-12-23 11:54:58 -0600

    Embed all the screen on a Stack to add a background custom color and ignore the edges for the color to use the whole space

--- BullsEye/Views/ContentView.swift
+++ BullsEye/Views/ContentView.swift
@@ -13,51 +13,58 @@ struct ContentView: View {
   @State private var game = Game()
 
   var body: some View {
-    VStack {
-      Text("🎯🎯🎯\nPut the Bull's Eye as close as you can to".uppercased())
-        .bold()
-        .kerning(2.0)
-        .multilineTextAlignment(.center)
-        .lineSpacing(4.0)
-        .font(.footnote)
-      Text("\(game.target)")
-        .fontWeight(.black)
-        .kerning(-1.0)
-        .font(.largeTitle)
-      HStack {
-        Text("1")
-          .bold()
-        Slider(value: $sliderValue, in: 1.0...100.0)
-        Text("100")
-          .bold()
-      }
-      .padding()
+    ZStack {
+      Color(red: 243.0 / 255.0, green: 248 / 255.0, blue: 253 / 255.0, opacity: 1.0)
+        .edgesIgnoringSafeArea(.all)
 
-      Button(
-        action: {
-          print("Hello, SwiftUI!")
-          alertIsVisible = true
-        },
-        label: {
-          Text("Hit me!".uppercased())
+      VStack {
+        Text("🎯🎯🎯\nPut the Bull's Eye as close as you can to".uppercased())
+          .bold()
+          .kerning(2.0)
+          .multilineTextAlignment(.center)
+          .lineSpacing(4.0)
+          .font(.footnote)
+          .padding(.leading, 30.0)
+          .padding(.trailing, 30.0)
+        Text("\(game.target)")
+          .fontWeight(.black)
+          .kerning(-1.0)
+          .font(.largeTitle)
+        HStack {
+          Text("1")
             .bold()
-            .font(.title3)
+          Slider(value: $sliderValue, in: 1.0...100.0)
+          Text("100")
+            .bold()
         }
-      )
-      .padding(20.0)
-      .background(Color.blue)
-      .foregroundColor(.white)
-      .cornerRadius(21.0)
-      .alert(isPresented: $alertIsVisible) {
-        let roundedValue = Int(sliderValue.rounded())
-        return Alert(
-          title: Text("Hello there!"),
-          message: Text(
-            "The slider's value is \(roundedValue).\n"
-              + "You scored \(game.points(sliderValue: roundedValue)) points this round"
-          ),
-          dismissButton: .default(Text("Awesome!"))
+        .padding()
+
+        Button(
+          action: {
+            print("Hello, SwiftUI!")
+            alertIsVisible = true
+          },
+          label: {
+            Text("Hit me!".uppercased())
+              .bold()
+              .font(.title3)
+          }
         )
+        .padding(20.0)
+        .background(Color.blue)
+        .foregroundColor(.white)
+        .cornerRadius(21.0)
+        .alert(isPresented: $alertIsVisible) {
+          let roundedValue = Int(sliderValue.rounded())
+          return Alert(
+            title: Text("Hello there!"),
+            message: Text(
+              "The slider's value is \(roundedValue).\n"
+                + "You scored \(game.points(sliderValue: roundedValue)) points this round"
+            ),
+            dismissButton: .default(Text("Awesome!"))
+          )
+        }
       }
     }
   }
