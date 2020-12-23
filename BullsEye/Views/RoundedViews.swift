a3aca09ad46dc9bc486e5de1a92e343e5dcc8d81
Juan Dorado <fdorado985@gmail.com>
2020-12-23 15:24:09 -0600

    Create the two types of rounded views, fill one and a stroke one and their colors to use each

--- BullsEye/Views/RoundedViews.swift
+++ BullsEye/Views/RoundedViews.swift
@@ -15,6 +15,10 @@ struct RoundedImageViewStroked: View {
       .font(.title)
       .foregroundColor(Color("TextColor"))
       .frame(width: 56.0, height: 56.0)
+      .overlay(
+        Circle()
+          .strokeBorder(Color("ButtonStrokeColor"), lineWidth: 2.0)
+      )
   }
 }
 
@@ -24,17 +28,27 @@ struct RoundedImageViewFilled: View {
   var body: some View {
     Image(systemName: systemName)
       .font(.title)
-      .foregroundColor(Color("TextColor"))
+      .foregroundColor(Color("ButtonFilledTextColor"))
       .frame(width: 56.0, height: 56.0)
+      .background(
+        Circle()
+          .fill(Color("ButtonFilledBackgroundColor"))
+      )
   }
 }
 
 struct PreviewView: View {
   var body: some View {
     VStack(spacing: 10) {
-      RoundedImageViewStroked(systemName: "arrow.counterclockwise")
-
+      HStack {
+        RoundedImageViewStroked(systemName: "arrow.counterclockwise")
         RoundedImageViewStroked(systemName: "list.dash")
+      }
+
+      HStack {
+        RoundedImageViewFilled(systemName: "arrow.counterclockwise")
+        RoundedImageViewFilled(systemName: "list.dash")
+      }
     }
   }
 }
