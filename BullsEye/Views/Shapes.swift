31badd94fc3d49ee4bc43fbbe6f04799b9d1acdd
Juan Dorado <fdorado985@gmail.com>
2020-12-24 01:18:44 -0600

    Create default animations to all the shapes

--- BullsEye/Views/Shapes.swift
+++ BullsEye/Views/Shapes.swift
@@ -8,30 +8,42 @@
 import SwiftUI
 
 struct Shapes: View {
-    var body: some View {
-      VStack {
+  @State private var wideShapes = true
+
+  var body: some View {
+    VStack {
+      if !wideShapes {
         Circle()
           .strokeBorder(Color.blue, lineWidth: 20.0)
           .frame(width: 200, height: 100)
-
-        RoundedRectangle(cornerRadius: 20.0)
-          .fill(Color.blue)
-          .frame(width: 200.0, height: 100.0)
-
-        Capsule()
-          .fill(Color.blue)
-          .frame(width: 200.0, height: 100.0)
-
-        Ellipse()
-          .fill(Color.blue)
-          .frame(width: 200, height: 100)
       }
-      .background(Color.green)
+
+      RoundedRectangle(cornerRadius: 20.0)
+        .fill(Color.blue)
+        .frame(width: wideShapes ? 200.0 : 100, height: 100.0)
+
+      Capsule()
+        .fill(Color.blue)
+        .frame(width: wideShapes ? 200.0 : 100, height: 100.0)
+
+      Ellipse()
+        .fill(Color.blue)
+        .frame(width: wideShapes ? 200.0 : 100, height: 100)
+
+      Button(action: {
+        withAnimation {
+          wideShapes.toggle()
+        }
+      }, label: {
+        Text("Animate!")
+      })
     }
+    .background(Color.green)
+  }
 }
 
 struct Shapes_Previews: PreviewProvider {
-    static var previews: some View {
-        Shapes()
-    }
+  static var previews: some View {
+    Shapes()
+  }
 }
